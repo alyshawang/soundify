4f233d4c14ec290759cf2aef6c78871f6005aa57
alyshawang <113640728+alyshawang@users.noreply.github.com>
2023-07-31 19:00:44 -0400

    multiple

--- components/Home/Home.jsx
+++ components/Home/Home.jsx
@@ -1,51 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import { fetchUserPlaylists } from '../spotifyAPI'; // Import the function
-import PlaylistConversionButton from '../playlist'; // Replace the path with the actual path to your component
 
-
-function extractAccessToken() {
-  const storedAccessToken = localStorage.getItem('accessToken');
-  if (storedAccessToken) {
-    return storedAccessToken;
-  }
-
-  const hash = window.location.hash.substring(1);
-  const params = new URLSearchParams(hash);
-  return params.get('access_token');
-}
-
-export default function Home() {
-  const [playlists, setPlaylists] = useState([]);
-
-  useEffect(() => {
-    // Call the fetchUserPlaylists function and pass the access token to it
-    const accessToken = extractAccessToken();
-    fetchUserPlaylists(accessToken)
-      .then((userPlaylists) => {
-        setPlaylists(userPlaylists);
-      })
-      .catch((error) => {
-        console.error('Error fetching playlists:', error);
-      });
-  }, []);
-
-  return (
-    <div>
-      {/* Display the playlists */}
-      <h2>Your Playlists</h2>
-      <ul>
-        {playlists.map((playlist) => (
-          <li key={playlist.id}>{playlist.name}</li>
-        ))}
-      </ul>
-      <PlaylistConversionButton />
-    </div>
-  );
-}
 
 // import React, { useEffect, useState } from 'react';
-// import { fetchUserPlaylists } from '../spotifyAPI';
+// import { fetchUserPlaylists } from '../spotifyAPI'; // Import the function
 // import PlaylistConversionButton from '../playlist'; // Replace the path with the actual path to your component
+// import styles from "./Home.module.css";
 
 // function extractAccessToken() {
 //   const storedAccessToken = localStorage.getItem('accessToken');
@@ -73,29 +31,90 @@ export default function Home() {
 //       });
 //   }, []);
 
-//   // Function to handle playlist conversion for a specific playlist
-//   const handleConvertPlaylist = (playlistId) => {
-//     axios.post('http://127.0.0.1:5000/convert_playlist', { spotifyPlaylistId: playlistId })
-//       .then((response) => {
-//         console.log(response.data.message);
-//       })
-//       .catch((error) => {
-//         console.error('Error converting playlist:', error);
-//       });
-//   };
-
 //   return (
 //     <div>
 //       {/* Display the playlists */}
 //       <h2>Your Playlists</h2>
 //       <ul>
 //         {playlists.map((playlist) => (
-//           <li key={playlist.id}>
-//             {playlist.name}
-//             <PlaylistConversionButton playlistId={playlist.id} />
+//           <li key={playlist.id} className={styles.playlistitem}>
+//             {playlist.image && (
+//               <a href={`https://open.spotify.com/playlist/${playlist.id}`} target="_blank" rel="noopener noreferrer">
+//                 <img src={playlist.image} alt={playlist.name} className={styles.playlistimage} />
+//               </a>
+//             )}
+//             <a href={`https://open.spotify.com/playlist/${playlist.id}`} target="_blank" rel="noopener noreferrer">
+//               {playlist.name}
+//             </a>
 //           </li>
 //         ))}
 //       </ul>
+//       <PlaylistConversionButton />
 //     </div>
 //   );
 // }
+import React, { useEffect, useState } from 'react';
+import { fetchUserPlaylists } from '../spotifyAPI'; // Import the function
+import PlaylistConversionButton from '../playlist'; // Replace the path with the actual path to your component
+import styles from "./Home.module.css";
+import axios from 'axios';
+
+function extractAccessToken() {
+  const storedAccessToken = localStorage.getItem('accessToken');
+  if (storedAccessToken) {
+    return storedAccessToken;
+  }
+
+  const hash = window.location.hash.substring(1);
+  const params = new URLSearchParams(hash);
+  return params.get('access_token');
+}
+
+export default function Home() {
+  const [playlists, setPlaylists] = useState([]);
+
+  useEffect(() => {
+    // Call the fetchUserPlaylists function and pass the access token to it
+    const accessToken = extractAccessToken();
+    fetchUserPlaylists(accessToken)
+      .then((userPlaylists) => {
+        setPlaylists(userPlaylists);
+      })
+      .catch((error) => {
+        console.error('Error fetching playlists:', error);
+      });
+  }, []);
+
+  const handleConvertPlaylist = (playlistId) => {
+    axios.post('http://127.0.0.1:5000/convert_playlist', { playlistId })
+      .then((response) => {
+        console.log(response.data.message);
+      })
+      .catch((error) => {
+        console.error('Error converting playlist:', error);
+      });
+  };
+
+  return (
+    <div>
+      {/* Display the playlists */}
+      <h2>Your Playlists</h2>
+      <ul>
+        {playlists.map((playlist) => (
+          <li key={playlist.id} className={styles.playlistitem}>
+            {playlist.image && (
+              <a href={`https://open.spotify.com/playlist/${playlist.id}`} target="_blank" rel="noopener noreferrer">
+                <img src={playlist.image} alt={playlist.name} className={styles.playlistimage} />
+              </a>
+            )}
+            <a href={`https://open.spotify.com/playlist/${playlist.id}`} target="_blank" rel="noopener noreferrer">
+              {playlist.name}
+            </a>
+            <button onClick={() => handleConvertPlaylist(playlist.id)}>Convert to YouTube Playlist</button>
+          </li>
+        ))}
+      </ul>
+      {/* <PlaylistConversionButton /> */}
+    </div>
+  );
+}
--- components/spotifyAPI.js
+++ components/spotifyAPI.js
@@ -1,3 +1,25 @@
+// export async function fetchUserPlaylists(accessToken) {
+//   try {
+//     const response = await fetch('https://api.spotify.com/v1/me/playlists', {
+//       headers: {
+//         Authorization: `Bearer ${accessToken}`,
+//       },
+//     });
+
+//     if (!response.ok) {
+//       throw new Error('Failed to fetch playlists');
+//     }
+
+//     const data = await response.json();
+//     const playlists = data.items; // Assuming data.items contains the playlists array
+
+//     return playlists;
+//   } catch (error) {
+//     console.error('Error fetching playlists:', error);
+//     throw error;
+//   }
+// }
+
 export async function fetchUserPlaylists(accessToken) {
   try {
     const response = await fetch('https://api.spotify.com/v1/me/playlists', {
@@ -11,7 +33,11 @@ export async function fetchUserPlaylists(accessToken) {
     }
 
     const data = await response.json();
-    const playlists = data.items; // Assuming data.items contains the playlists array
+    const playlists = data.items.map((playlist) => ({
+      id: playlist.id,
+      name: playlist.name,
+      image: playlist.images.length ? playlist.images[0].url : null, // Get the first image of the playlist, if available
+    }));
 
     return playlists;
   } catch (error) {
@@ -19,3 +45,26 @@ export async function fetchUserPlaylists(accessToken) {
     throw error;
   }
 }
+
+// export async function fetchUserPlaylists(accessToken) {
+//   try {
+//     const response = await fetch('https://api.spotify.com/v1/me/playlists', {
+//       headers: {
+//         Authorization: `Bearer ${accessToken}`,
+//       },
+//     });
+
+//     if (!response.ok) {
+//       const errorData = await response.json();
+//       throw new Error(`Failed to fetch playlists: ${errorData.error.message}`);
+//     }
+
+//     const data = await response.json();
+//     const playlists = data.items;
+
+//     return playlists;
+//   } catch (error) {
+//     console.error('Error fetching playlists:', error);
+//     throw error;
+//   }
+// }
